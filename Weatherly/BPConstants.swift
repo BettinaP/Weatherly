bb6d034e37a389a40072dac58a48f498f7fea611
BettinaP <bettinaprophete@gmail.com>
2016-11-17 16:28:18 -0500

    initial hour models created

---
+++ Weatherly/BPConstants.swift
@@ -0,0 +1,13 @@
+//
+//  BPConstants.swift
+//  Weatherly
+//
+//  Created by Bettina on 11/17/16.
+//  Copyright © 2016 Bettina Prophete. All rights reserved.
+//
+
+//MARK: - Networking
+
+let kAPIKey = "f8fbe3960d76e28bb3b8355fbef45222"
+let kAPIWeatherForecast = "https://api.darksky.net/forecast/%@/%f,%f" //API Secret, Latitude, Longitude
+
